e1e6ac00873b48ced4bad55af306d442bf858608
Arijeet Dasgupta <arigofficial71@gmail.com>
2022-03-18 21:40:19 +0530

    Further functions added

--- App03/App03/StudentGrades.cs
+++ App03/App03/StudentGrades.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.Design;
 
 namespace App03
 {
@@ -10,6 +11,108 @@ namespace App03
     /// </summary>
     public class StudentGrades
     {
+        public int MenuChoice;
+        public int size;
 
+        public Tuple<String, double, Grades>[] StudentData;
+        /*public static void Main(string[] args)
+        {
+            Start();
+            //Input();
+        }*/
+
+        public void Start()
+        {
+            string[] menu =
+            {
+                "Input Marks",
+                "Output Marks",
+                "Output Stats",
+                "Output Grade Profile",
+                "Quit"
+            };
+            
+            MenuChoice = Utilities.SelectChoice(menu);
+            choiceRouter(MenuChoice);
+        }
+
+        public void choiceRouter(int MenuChoice)
+        {
+            switch (MenuChoice)
+            {
+                case 1: Input();
+                    break;
+                case 2: OutputMarks();
+                    break;
+                case 3: OutputStats();
+                    break;
+                case 4: OutputGradeProfile();
+                    break;
+                case 5: Quit();
+                    break;
+                default: Console.WriteLine("Wrong Option Entered! ");
+                    break;
+            }
+                
+        }
+
+        public void Input()
+        {
+            Console.WriteLine("Enter the numbers of students: ");
+            size = Convert.ToInt32(Console.ReadLine());
+            StudentData = new Tuple<string, double, Grades>[size];
+
+            for (int i = 0; i < size; i++)
+            {
+                Console.WriteLine("Enter name: ");
+                String name = Console.ReadLine();
+                double marks = Utilities.Input("Enter Marks: ");
+                StudentData[i] = Tuple.Create(name, marks, CalculateGrade(marks));
+            }
+            OutputMarks();
+        }
+
+        public Grades CalculateGrade(double marks)
+        {
+            Grades grade= Grades.F;
+            if (marks >= 70 && marks <= 100)
+                grade = Grades.A;
+            else if(marks >=60 && marks<=69)
+                grade =Grades.B;
+            else if(marks >=50 && marks<=59)
+                grade = Grades.C;
+            else if(marks >=40 && marks<=49)
+                grade = Grades.D;
+            else 
+                grade = Grades.F;
+
+            return grade;
+        }
+        
+        
+
+        public void OutputMarks()
+        {
+            Console.WriteLine("Name\t\tMarks\t\tGrade\t\tClassification");
+            foreach (var i in StudentData)
+            {
+                Console.WriteLine(i.Item1+ "\t\t" +i.Item2+"\t\t" + i.Item3 + "\t\t" + i.Item3.GetEnumDescription());
+            }
+        }
+
+        public void OutputStats()
+        {
+            
+        }
+
+        public void OutputGradeProfile()
+        {
+            
+        }
+
+        public void Quit()
+        {
+            
+        }
     }
 }
