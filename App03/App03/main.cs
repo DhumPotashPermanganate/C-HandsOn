c81d41c989595ef7c36acde3c9d4b13254c74883
Arijeet Dasgupta <arigofficial71@gmail.com>
2022-03-18 21:41:52 +0530

    Main

---
+++ App03/App03/main.cs
@@ -0,0 +1,11 @@
+﻿namespace App03;
+
+public class main
+{
+    public static void Main(string[] args)
+    {
+        StudentGrades obj = new StudentGrades();
+        obj.Start();
+        //Input();
+    }
+}
