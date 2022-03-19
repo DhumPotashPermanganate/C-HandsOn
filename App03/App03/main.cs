aae83e0feaceb9560fe492dc9d6754b2e309e7eb
Arijeet Dasgupta <arigofficial71@gmail.com>
2022-03-20 01:55:00 +0530

    Final Touches

--- App03/App03/main.cs
+++ App03/App03/main.cs
@@ -4,8 +4,16 @@ public class main
 {
     public static void Main(string[] args)
     {
+        Console.ForegroundColor = ConsoleColor.Green;
+
+        Console.WriteLine();
+        Console.WriteLine(" =================================================");
+        Console.WriteLine("    BNU CO453 Applications Programming 2021-2022! ");
+        Console.WriteLine("                 Irtaza Qayyum                               \n " +
+                          "                    22136251 ");
+        Console.WriteLine(" =================================================");
+        Console.WriteLine();
         StudentGrades obj = new StudentGrades();
         obj.Start();
-        //Input();
     }
 }
